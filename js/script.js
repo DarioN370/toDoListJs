2b484248a90bdc08061e22784c2f06c4a9c19913
DarioN370 <netobis2015@gmail.com>
2024-12-14 19:52:49 -0300

    corrigindo o erro dos campos vazios serem adicionados em branco, coloquei tudo dentro de uma estrutura IF ELSE para que o elemento da UL com LI só seja adicionado caso o campo 'digite sua tarefa' esteja preenchido, caso contrário sera exibida uma mensagem de erro e o campo não sera criado

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 function adicionarTarefa(){
-    let mensagem = "Tarefa adicionada com sucesso !!!";  // definindo a variavel mensagem com a mensagem que eu quero 
-   
 
     let inputTarefa = document.getElementById("inputTarefa"); //aqui eu coloco que o inpuTarefa vai pegar o valor do input
     let tarefa = inputTarefa.value; //aqui eu pego o que o user digitou no meu input e atribuo ele na variavel tarefa 
-    document.getElementById("mensagem").textContent = mensagem; //aqui eu estou exibindo a tarefa que o user digitou
+    
 
-    let listaTarefas = document.getElementById("listaTarefas") //aqui ele pega o elemento com o id listaTarefas e armazena na var listaTarefas
-    let novaTarefa = document.createElement("li") //vou criar o elemento que esta entre parenteses
+    // se o valor do input for vazio, então mostre uma mensagem de erro para o user 
+    if (tarefa == ""){// duas aspas é considerado vazio  -  Sinal de dois iguais é pra comparar igualdade
+        let mensagemErro = "Digite uma tarefa para adiciona-la !!!!!!";  // definindo a variavel mensagem com a mensagem que eu quero 
+        document.getElementById("mensagem").textContent = mensagemErro  //aqui eu estou exibindo que a tarefa precisa ser adicionada
+    } else {
+        let mensagemSucesso = "Tarefa adicionada com sucesso.";  // definindo a variavel mensagem com a mensagem que eu quero 
+        document.getElementById("mensagem").textContent = mensagemSucesso; //aqui eu estou exibindo que a tarefa foi adicionada
 
-    novaTarefa.textContent = tarefa //aqui eu pego o novaTarefa e peço pra ele escrever com o textContent a variavel tarefa, assim ele vai listar certo
+        let listaTarefas = document.getElementById("listaTarefas"); //aqui ele pega o elemento com o id listaTarefas e armazena na var listaTarefas
+        let novaTarefa = document.createElement("li"); //vou criar o elemento que esta entre parenteses
+        novaTarefa.textContent = tarefa; //aqui eu pego o novaTarefa e peço pra ele escrever com o textContent a variavel tarefa, assim ele vai listar certo
+        listaTarefas.appendChild(novaTarefa); //aqui eu peço para que o elemento pai no caso a UL, crie elementos filhos, que são as LI
+    }
 
-    listaTarefas.appendChild(novaTarefa) //aqui eu peço para que o elemento pai no caso a UL, crie elementos filhos, que são as LI
+
+    
+
+
+    
+
 
 
 
 
     inputTarefa.value = ""; //aqui eu peço para depois que user digitar, e dps que o codigo rodar, eu mando ele esvaziar o campo de input
-} 
+};
